2f2e6f87039bfb14ea83efce859ef84548a96a09
Libe <mrlibelula@gmail.com>
2024-06-23 17:18:05 -0400

    Console short name on explore cards

--- resources/views/livewire/game-card-classic.blade.php
+++ resources/views/livewire/game-card-classic.blade.php
@@ -1,7 +1,13 @@
-<button class="group rounded-lg bg-center bg-cover bg-no-repeat border-[3px] shadow-md shadow-cod-gray-500 dark:shadow-black  border-cod-gray-500 dark:border-cod-gray-700 opacity-75 hover:brightness-110 smooth-300 cursor-pointer" 
+<button class="group rounded-lg overflow-hidden bg-center bg-cover bg-no-repeat border-[3px] shadow-md shadow-cod-gray-500 dark:shadow-black  border-cod-gray-500 dark:border-cod-gray-700 opacity-75 hover:brightness-110 smooth-300 cursor-pointer" 
     style="background-image: url({{ $game['poster'] }})"
 >
     <div class="flex items-end justify-center w-[8.5rem] h-[12rem] overflow-hidden">
+        @isset($game['console_short_name'])
+        <div class=" w-full bg-white/70 dark:bg-cod-gray-700/70 dark:text-gray-200 font-mono text-xs py-0.5 font-semibold">
+            {{ $game['console_short_name'] }}
+        </div>
+        @else
         &nbsp;
+        @endisset
     </div>
 </button>
