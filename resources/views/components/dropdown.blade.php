4d52ea3f6eec5556260d2ac9a610048134d5f9c7
Libe <mrlibelula@gmail.com>
2026-05-08 23:05:36 -0400

    UI fixes

--- resources/views/components/dropdown.blade.php
+++ resources/views/components/dropdown.blade.php
@@ -37,7 +37,7 @@ switch ($width) {
             x-transition:leave="transition ease-in duration-75"
             x-transition:leave-start="transform opacity-100 scale-100"
             x-transition:leave-end="transform opacity-0 scale-95"
-            class="absolute z-50 mt-2 {{ $width }} rounded-md shadow-lg {{ $alignmentClasses }} {{ $dropdownClasses }}"
+            class="absolute z-50 mt-2 {{ $width }} rounded-lg overflow-hidden border-[3px] border-rose-400 dark:border-rose-600 shadow-lg shadow-black/20 dark:shadow-black/60 {{ $alignmentClasses }} {{ $dropdownClasses }}"
             style="display: none;"
             @click="open = false">
         <div class="rounded-md ring-1 ring-black ring-opacity-5 {{ $contentClasses }}">
